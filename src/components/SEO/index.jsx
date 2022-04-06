8e3969ff634464b982927bab2aaea49f9fb8ca87
ysl <6h15m.develop@gmail.com>
2022-04-06 23:40:51 +0900

    feat: twitter meta 유형 변경

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -10,7 +10,7 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:image" content={`${siteUrl}/og-image.png`} />
