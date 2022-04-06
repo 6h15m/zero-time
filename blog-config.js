24fb406296ccbcc54fff3b34d4ca4e75a141ac63
ysl <6h15m.develop@gmail.com>
2022-04-06 19:14:51 +0900

    fix: 이메일 링크 복구

--- blog-config.js
+++ blog-config.js
@@ -8,7 +8,7 @@ module.exports = {
     linkedIn:
       "https://www.linkedin.com/in/%EC%98%88%EC%84%9C-%EC%9D%B4-9b0a3a213/",
     facebook: "https://www.facebook.com/profile.php?id=100007430927430",
-    email: "6h15m.develop@gmail.com",
+    email: "mailto:6h15m.develop@gmail.com",
   },
   utterances: {
     repo: "6h15m/6h15m.github.io",
