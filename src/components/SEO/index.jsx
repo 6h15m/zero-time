a234add06b5aba48fec8d1e11133a84427f04f68
ysl <6h15m.develop@gmail.com>
2022-04-06 23:37:49 +0900

    fix: og image 경로 변경

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -8,12 +8,12 @@ const SEO = ({ title, description, url }) => {
       <title>{title}</title>
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta property="og:image" content={`${siteUrl}og-image.png`} />
       {description && <meta name="description" content={description} />}
       <meta name="twitter:card" content="summary" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
-      <meta property="twitter:image" content={`${siteUrl}/og-image.png`} />
+      <meta property="twitter:image" content={`${siteUrl}og-image.png`} />
       {description && <meta name="twitter:description" content={description} />}
     </Helmet>
   );
