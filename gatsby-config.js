1f82bb53e53af69b9448f80d166e63a25e690d49
ysl <6h15m.develop@gmail.com>
2022-04-07 16:56:02 +0900

    chore: dependency 업그레이드

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-catch-links`,
-    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-plugin-react-redux`,
       options: {
@@ -24,16 +23,6 @@ module.exports = {
         },
         cleanupOnClient: true,
         windowKey: "__PRELOADED_STATE__",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `noto sans kr:300,400,500,700,900`,
-          `source code pro:700`, // you can also specify font weights and styles
-        ],
-        display: "swap",
       },
     },
     "gatsby-plugin-styled-components",
@@ -122,60 +111,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-resolve-src`,
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map((edge) => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                });
-              });
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { slug }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: `/rss.xml`,
-            title: `RSS Feed of ${title}`,
-            match: "^/blog/",
-          },
-        ],
-      },
-    },
   ],
 };
