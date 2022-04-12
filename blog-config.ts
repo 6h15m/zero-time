5aa7d9e902f8760ddcaba93f0a309c8a2394a7c3
ysl <6h15m.develop@gmail.com>
2022-04-12 17:35:43 +0900

    fix: 이메일 정보 변경

--- blog-config.ts
+++ blog-config.ts
@@ -22,7 +22,7 @@ const blogConfig: BlogConfig = {
     github: "https://github.com/6h15m",
     linkedIn:
       "https://www.linkedin.com/in/%EC%98%88%EC%84%9C-%EC%9D%B4-9b0a3a213/",
-    email: "mailto:6h15m.develop@gmail.com",
+    email: "mailto:mkitwave@gmail.com",
   },
   utterances: {
     repo: "6h15m/6h15m.github.io",
